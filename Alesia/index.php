ba62de3165cf145277e63c19308296c378d0221b
Laurent Robidoux <laurent.robidoux.cem@gmail.com>
2019-09-20 16:37:25 -0400

    alesia

---
+++ Alesia/index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<link rel="stylesheet" href="/css/bootstrap.min.css">
+<link rel="stylesheet" href="/css/ohrizon.css">
+<script src="/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+<div class="center">
+<h1 class="title">Ohrizon - Alesia</h1>
+<a class="btn btn-light"href="itms-services://?action=download-manifest&url=https://ohrizonapple.herokuapp.com/Alesia/manifest.plist">
+    Télécharger
+</a>
+</div>
+
+
+</body>
+
+</html>
+
